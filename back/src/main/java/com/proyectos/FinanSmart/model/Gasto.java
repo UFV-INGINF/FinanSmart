8b261c21431c56f29bd18b93e3bfabcaf5b28904
Luis <luiseduardodesantiago.u@gmail.com>
2025-03-06 16:48:47 +0100

    Cambios en gasto para volver a la anterior estructura de codigo

--- back/src/main/java/com/proyectos/FinanSmart/model/Gasto.java
+++ back/src/main/java/com/proyectos/FinanSmart/model/Gasto.java
@@ -1,38 +1,13 @@
 package com.proyectos.FinanSmart.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "gastos")
+@Data
 public class Gasto {
-    private String id;
-    private int monto;
 
-    // Constructor
-    public Gasto(String id, int monto) {
-        this.id = id;
-        this.monto = monto;
-    }
-
-    // Getter n Setter
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public int getMonto() {
-        return monto;
-    }
-
-    public void setMonto(int monto) {
-        this.monto = monto;
-    }
-
-    // To String
-    @Override
-    public String toString() {
-        return "Gasto{" +
-                "id='" + id + '\'' +
-                ", monto=" + monto +
-                '}';
-    }
 }
