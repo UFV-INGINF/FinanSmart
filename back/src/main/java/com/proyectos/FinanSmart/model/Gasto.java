a1ddadaf080b7f3b5ae0196f935220bacbb58cd2
Luis <luiseduardodesantiago.u@gmail.com>
2025-03-06 16:49:37 +0100

    Final de la clase gasto con la implementación de lombok

--- back/src/main/java/com/proyectos/FinanSmart/model/Gasto.java
+++ back/src/main/java/com/proyectos/FinanSmart/model/Gasto.java
@@ -9,5 +9,19 @@ import java.time.LocalDateTime;
 @Table(name = "gastos")
 @Data
 public class Gasto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //  la base de datos generará automáticamente el valor del id (autoincremental).
 
+    private int id;
+    private Double monto;
+    private String categoria;
+    private LocalDateTime fecha_gasto;
+
+    // Relación con Usuario (Muchos ingresos pueden pertenecer a un usuario)
+    @ManyToOne
+    //"usuario_id" será el nombre de la columna en la tabla gasto.
+    // nullable = false indica que un ingreso no puede existir sin un usuario.
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
 }
